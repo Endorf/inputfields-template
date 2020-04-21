17d22d94b91e0e71ef171c54d0c7b6a9ca55cf6a
Dmytro Kos <dmitriy.kosolapenko@gmail.com>
2020-04-21 11:10:36 +0300

    initial commit

---
+++ app/src/main/java/com/inputfieldtemplate/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.inputfieldtemplate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id) {
+            R.id.btn -> handleBtnClick()
+        }
+    }
+
+    private fun handleBtnClick() {
+
+    }
+}
